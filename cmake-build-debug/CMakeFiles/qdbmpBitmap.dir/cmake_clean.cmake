f6971902cadfffea2994cf799b2c50deed1128e2
Unknown <adamsha.asif@gmail.com>
2018-07-05 13:44:13 +0200

    Decoding bmp image ok

---
+++ cmake-build-debug/CMakeFiles/qdbmpBitmap.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qdbmpBitmap.dir/main.c.obj"
+  "CMakeFiles/qdbmpBitmap.dir/qdbmp.c.obj"
+  "qdbmpBitmap.pdb"
+  "qdbmpBitmap.exe"
+  "qdbmpBitmap.exe.manifest"
+  "libqdbmpBitmap.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/qdbmpBitmap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
